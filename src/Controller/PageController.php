fa111a42b33ceb6d6f795ff6612626c5c2c09028
joaomarcelo <joaomarcelo@jmachine.ok>
2022-04-26 09:22:29 -0300

    adição de controller generico e primeiras paginas html

---
+++ src/Controller/PageController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Source\Controller;
+
+use \Source\UI\View;
+
+class PageController {
+
+  /**
+   * método responsável por retornar o conteúdo do footer da página
+   * @return string
+   */
+  private static function getFooter(): string {
+    return View::render('pages/footer');
+  }
+
+  /**
+   * método responsável por retornar o conteúdo do header da página
+   * @param  string $title
+   * @return string
+   */
+  private static function getHeader($title): string {
+    return View::render('pages/header', [
+      'title' => $title
+    ]);
+  }
+  /**
+   * método responsável por retornar o conteúdo da view genérica
+   * @param  string $title
+   * @param  string $content
+   * @return string
+   */
+  public static function getPage($title, $content): string {
+    return View::render('pages/page', [
+      'title'   => $title,
+      'header'  => self::getHeader($title),
+      'content' => $content,
+      'footer'  => self::getFooter()
+    ]);
+  }
+
+}
